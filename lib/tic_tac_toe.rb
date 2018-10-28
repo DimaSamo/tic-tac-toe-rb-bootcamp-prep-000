ce5103d3aafe2e7849e6abf2cd99167d8047338f
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-28 17:15:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,5 +7,12 @@ WIN_COMBINATIONS = [
   [2,5,8],  # Right column
   [0,4,8],  # Horizontal 1-8
   [2,4,6]  # Horizontal 2-6
-  # ETC, an array for each win combination
 ]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
